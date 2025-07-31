b25ad53851c053b236f3b29209122533e249c574
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-08-01 00:45:23 +0700

    feat: Restyle modal

--- src/components/confirm-modal/index.tsx
+++ src/components/confirm-modal/index.tsx
@@ -1,9 +1,9 @@
-import { Button, Image, Modal } from "antd";
+import { Button, Flex, Image, Modal, Space } from "antd";
 import React from "react";
 import "./confirm-modal.scss";
 
-import ErrorIcon from "../../images/ic_error.png";
-import InfoIcon from "../../images/ic_info.png";
+import ErrorIcon from "../../assets/ic_error.png";
+import InfoIcon from "../../assets/ic_info.png";
 
 const ConfirmModal: React.FC<{
   showModal: boolean;
@@ -30,32 +30,17 @@ const ConfirmModal: React.FC<{
       open={showModal}
       footer={null}
       onCancel={onCancel}
-      width={450}
-      bodyStyle={{
-        display: "flex",
-        flexFlow: "column",
-        alignItems: "center",
-        padding: 32,
-      }}
+      width={350}
     >
-      <Image
-        src={danger ? ErrorIcon : InfoIcon}
-        className="confirm-icon"
-        preview={false}
-      />
-      <div className="confirm-title">{title}</div>
-      <div className="confirm-des">{description}</div>
-      {showButtons && (
-        <div className="confirm-btn-container">
-          <Button
-            type="primary"
-            block
-            className="confirm-cancel-btn"
-            onClick={onCancel}
-          >
-            Cancel
-          </Button>
-          <div className="confirm-btn-space" />
+      <Flex vertical align="center" justify="stretch" gap={12}>
+        <Image
+          src={danger ? ErrorIcon : InfoIcon}
+          className="confirm-icon"
+          preview={false}
+        />
+        <div className="confirm-title">{title}</div>
+        <div className="confirm-des">{description}</div>
+        {showButtons && (
           <Button
             type="primary"
             block
@@ -67,8 +52,13 @@ const ConfirmModal: React.FC<{
           >
             {okText}
           </Button>
-        </div>
-      )}
+        )}
+        {showButtons && (
+          <Button block onClick={onCancel}>
+            Cancel
+          </Button>
+        )}
+      </Flex>
     </Modal>
   );
 };
