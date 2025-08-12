8f9d6121c9406d6bd033d5435d7798c3340abc51
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-08-13 01:37:51 +0700

    feat: Path to build prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/chainforge/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
