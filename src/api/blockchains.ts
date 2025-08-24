3179542e7d18a5aed0c751960cd1b66365ae7ccb
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-08-24 22:39:12 +0700

    feat: Add cors credentials

--- src/api/blockchains.ts
+++ src/api/blockchains.ts
@@ -2,6 +2,9 @@ import { Blockchain } from "@utils/constants";
 import { CHAINFORGE_API } from "@api/constants";
 
 export const fetchBlockchains = async (): Promise<Blockchain[]> => {
-  const response = await fetch(`${CHAINFORGE_API}/blockchains`);
+  const response = await fetch(`${CHAINFORGE_API}/blockchains`, {
+    method: "GET",
+    credentials: "include",
+  });
   return await response.json();
 };
