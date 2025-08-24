8c9ba70590c70b6a56acd9065c6ef1fe841f5c5d
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-08-24 22:14:55 +0700

    feat: Use ChainForge BE

--- src/api/blockchains.ts
+++ src/api/blockchains.ts
@@ -1,28 +1,7 @@
 import { Blockchain } from "@utils/constants";
-import { DATABASE, TABLE } from "@api/constants";
+import { CHAINFORGE_API } from "@api/constants";
 
 export const fetchBlockchains = async (): Promise<Blockchain[]> => {
-  const response = await fetch(`https://opensheet.elk.sh/${DATABASE}/${TABLE}`);
-  const chains: Record<string, any>[] = await response.json();
-  return chains.map((chain) => {
-    const blockchain: Blockchain = {
-      id: chain.id,
-      code: chain.code,
-      chainId: chain.chain_id,
-      globalId: chain.global_id,
-      name: chain.name,
-      rpcUrl: chain.rpc_url,
-      explorerUrl: chain.explorer_url,
-      addressUrl: chain.address_url,
-      txUrl: chain.tx_url,
-      nativeDenom: chain.native_denom,
-      nativeToken: chain.native_token,
-      nativeDecimal: chain.native_decimal,
-      bech32Prefix: chain.bech32_prefix,
-      networkCluster: chain.network_cluster,
-      logo: chain.logo,
-      isTestnet: chain.is_testnet,
-    };
-    return blockchain;
-  });
+  const response = await fetch(`${CHAINFORGE_API}/blockchains`);
+  return await response.json();
 };
--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,4 +1,4 @@
-export const DATABASE = "1VRIuJVrgYDEZOf-UkIf1XInahUVXAASsI094r4aJSY8";
-export const TABLE = "blockchains";
+export const CHAINFORGE_API =
+  "https://chainforge-backend-smef.onrender.com/api"; // TODO: env config
 export const WEBHOOK =
   "https://discord.com/api/webhooks/1388933907277152256/qyMYIPSOjUxfkoLfIIqWlLiOXEStgIT2j1CM1VjUELjwbdki4FJMDlxzO-r4NkKc2h3t";
