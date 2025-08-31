e0885d69457a0aaea2b7310d85413fd1cb7e1618
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-08-31 19:52:17 +0700

    feat: Strictly handle content type

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -3,15 +3,22 @@ import { CHAINFORGE_API } from "./constants";
 export const makeRequest = async (
   endpoint: string,
   method: string,
-  body: any,
-  accessToken?: string
+  body?: any,
+  accessToken?: string,
+  contentType?: string
 ): Promise<any> => {
+  // Prepare headers
+  const headers: Record<string, string> = {
+    "Content-Type": "application/json",
+  }; // default content type
+  if (accessToken) headers["Authorization"] = `Bearer ${accessToken}`;
+  if (contentType) headers["Content-Type"] = contentType;
+
+  // Send request
   const response = await fetch(`${CHAINFORGE_API}${endpoint}`, {
     method: method,
-    headers: accessToken
-      ? { Authorization: `Bearer ${accessToken}` }
-      : undefined,
-    body: JSON.stringify(body),
+    headers: headers,
+    body: body ? JSON.stringify(body) : undefined,
     credentials: "include",
   });
   if (response.status >= 400) throw new Error(JSON.stringify(response.json()));
