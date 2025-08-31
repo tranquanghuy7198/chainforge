0c821a3390edeb7b358d38130b2c156a85c07e25
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-08-31 19:52:33 +0700

    feat: Define hook path

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "./src/components"),
       "@utils": path.resolve(__dirname, "./src/utils"),
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
     },
   },
   define: {
