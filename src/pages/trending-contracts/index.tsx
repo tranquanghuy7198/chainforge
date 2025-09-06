4ad8cc5740aeca08acdc3826eb22a6f67eee7598
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-09-06 19:50:54 +0700

    feat: Trending contract screen

---
+++ src/pages/trending-contracts/index.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import Header from "@components/header";
+import { DeployedContract, NetworkCluster } from "@utils/constants";
+import ContractCard from "@components/contract-card";
+import { capitalize } from "@utils/utils";
+import { XBlock, XMasonry } from "react-xmasonry";
+import { useFetchBlockchains } from "@hooks/blockchain";
+import { useFetchTrendingContracts } from "@/hooks/contract";
+
+const TrendingContracts: React.FC = () => {
+  const { blockchains } = useFetchBlockchains();
+  const { trendingContracts } = useFetchTrendingContracts();
+  const [displayedContracts, setDisplayedContracts] = useState<
+    DeployedContract[]
+  >([]);
+  const [selectedClusters, setSelectedClusters] = useState<string[]>([]);
+  const [searchedName, setSearchedName] = useState<string>();
+
+  useEffect(() => {
+    setDisplayedContracts(
+      trendingContracts.filter((contract) => {
+        if (
+          searchedName &&
+          !contract.template.name
+            .toLowerCase()
+            .includes(searchedName.toLowerCase())
+        )
+          return false;
+        for (const selectedNetworkCluster of selectedClusters)
+          if (
+            !blockchains
+              .filter((chain) =>
+                contract.addresses
+                  .map((address) => address.blockchainId)
+                  .includes(chain.id)
+              )
+              .map((chain) => chain.networkCluster.toString())
+              .includes(selectedNetworkCluster)
+          )
+            return false;
+        return true;
+      })
+    );
+  }, [trendingContracts, blockchains, selectedClusters, searchedName]);
+
+  return (
+    <div className="page">
+      <Header
+        header="Trending Contracts"
+        options={Object.values(NetworkCluster).map((cluster) => ({
+          value: cluster.toString(),
+          label: capitalize(cluster.toString()),
+        }))}
+        onSelected={setSelectedClusters}
+        onSearched={setSearchedName}
+        defaultSelectAll={false}
+      />
+      <div className="masonry-container">
+        <XMasonry center={false} targetBlockWidth={300}>
+          {displayedContracts.map((contract) => (
+            <XBlock key={contract.id}>
+              <ContractCard contract={contract} />
+            </XBlock>
+          ))}
+        </XMasonry>
+      </div>
+    </div>
+  );
+};
+
+export default TrendingContracts;
