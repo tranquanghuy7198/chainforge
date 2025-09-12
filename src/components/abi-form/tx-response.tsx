9a2d80ca96a37fa885a36dd610b11f20a3f0aa8c
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-09-12 12:02:42 +0700

    feat: Complete logic to link wallet

--- src/components/abi-form/tx-response.tsx
+++ src/components/abi-form/tx-response.tsx
@@ -1,31 +1,69 @@
 import { Alert, Button, Space } from "antd";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Blockchain, TX_PATTERN, TxResponse } from "@utils/constants";
 import { useAuth } from "@hooks/auth";
-import { checkWalletStatus } from "@api/auth";
+import { checkWalletStatus, linkWallet, requestChallenge } from "@api/auth";
 import { shorten } from "@utils/utils";
+import { Wallet } from "@utils/wallets/wallet";
+import useNotification from "antd/es/notification/useNotification";
 import "./abi-form.scss";
 
 const TransactionResult: React.FC<{
   blockchain?: Blockchain;
+  wallet: Wallet;
   txResponse: TxResponse;
-}> = ({ blockchain, txResponse }) => {
+}> = ({ blockchain, wallet, txResponse }) => {
+  const [notification, contextHolder] = useNotification();
   const { callAuthenticatedApi } = useAuth();
-  const [linked, setLinked] = React.useState<boolean>(false);
+  const [linked, setLinked] = useState<boolean>(true);
 
   useEffect(() => {
+    // If we cannot detect wallet, do not bother users
+    if (!wallet.address) {
+      setLinked(true);
+      return;
+    }
+
+    // Check wallet linking status
     callAuthenticatedApi(
       checkWalletStatus,
-      txResponse.walletAddress,
-      txResponse.networkCluster
+      wallet.address,
+      wallet.networkCluster
     ).then((linkStatus) => {
       if (linkStatus !== null) setLinked(linkStatus);
-      else setLinked(true); // If cannot detect linking status, do not bother users
+      else setLinked(true); // If we cannot detect linking status, do not bother users
     });
   }, [txResponse]);
 
+  const link = async () => {
+    try {
+      await wallet.connect();
+      if (!wallet.address)
+        throw new Error(`Cannot connect to ${wallet.ui.name} wallet`);
+      const [timestamp, nonce, challenge] = await requestChallenge(
+        wallet.address
+      );
+      const signature = await wallet.signMessage(challenge);
+      await callAuthenticatedApi(
+        linkWallet,
+        wallet.address,
+        timestamp,
+        nonce,
+        signature,
+        wallet.networkCluster
+      );
+      setLinked(false);
+    } catch (error) {
+      notification.error({
+        message: "Cannot connect wallet",
+        description: error instanceof Error ? error.message : String(error),
+      });
+    }
+  };
+
   return (
     <Space direction="vertical" className="tx-response">
+      {contextHolder}
       <Alert
         type="info"
         className="tx-response"
@@ -43,15 +81,15 @@ const TransactionResult: React.FC<{
           ))
         }
       />
-      {!linked && (
+      {wallet.address && !linked && (
         <Alert
           showIcon
           type="warning"
           message={`Wallet ${shorten(
-            txResponse.walletAddress
+            wallet.address
           )} is not connected to your account.`}
           action={
-            <Button size="small" type="text">
+            <Button size="small" type="text" onClick={link}>
               LINK
             </Button>
           }
