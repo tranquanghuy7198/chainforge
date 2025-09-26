274c511de19d55ed1e38a1e2fba6666667970103
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-09-26 11:24:10 +0700

    feat: Use makeRequest to fetch blockchains

--- src/api/blockchains.ts
+++ src/api/blockchains.ts
@@ -1,10 +1,6 @@
 import { Blockchain } from "@utils/constants";
-import { CHAINFORGE_API } from "@api/constants";
+import { makeRequest } from "@api/utils";
 
 export const fetchBlockchains = async (): Promise<Blockchain[]> => {
-  const response = await fetch(`${CHAINFORGE_API}/api/blockchains`, {
-    method: "GET",
-    credentials: "include",
-  });
-  return await response.json();
+  return await makeRequest("/api/blockchains", "GET");
 };
