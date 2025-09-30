9c975522743b360142f9b5bc0230f059c8b3a4f5
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-09-30 11:26:33 +0700

    feat: Make the error display better

--- src/components/abi-form/contract-call-error.tsx
+++ src/components/abi-form/contract-call-error.tsx
@@ -3,14 +3,17 @@ import React from "react";
 
 const ContractCallError: React.FC<{ error: any }> = ({ error }) => {
   return (
-    <Paragraph ellipsis={{ rows: 4, expandable: true, symbol: "View Full" }}>
-      Your smart contract call did not succeed. Please check that your RPC
-      endpoint is not rate-limited and verify your contract logic.
-      <br />
-      <strong>Detailed error:</strong>
-      <br />
-      {error instanceof Error ? error.message : String(error)}
-    </Paragraph>
+    <>
+      <Paragraph>
+        Your smart contract call did not succeed. Please check that your RPC
+        endpoint is not rate-limited and verify your contract logic.
+      </Paragraph>
+      <Paragraph ellipsis={{ rows: 4, expandable: true, symbol: "View Full" }}>
+        <strong>Detailed error:</strong>
+        <br />
+        {error instanceof Error ? error.message : String(error)}
+      </Paragraph>
+    </>
   );
 };
 
