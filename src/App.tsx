0fbcdb6587951b9591b4eb3b5770c67359ce22a2
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-10-09 17:14:45 +0700

    feat: Lazy loading to reduce Vite building packs

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./App.css";
 import { Provider } from "react-redux";
 import { store } from "@redux/store";
 import { ConfigProvider, theme } from "antd";
 import { PRIMARY_COLOR } from "@utils/constants";
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
-import Blockchains from "@pages/blockchains";
-import TrendingContracts from "@pages/trending-contracts";
-import ContractTemplates from "@pages/contract-templates";
-import Contracts from "@pages/contracts";
 import {
   BLOCKCHAINS,
   MY_CONTRACTS,
   MY_TEMPLATES,
   POPULAR_CONTRACTS,
 } from "@utils/routes";
+
+const Blockchains = lazy(() => import("@pages/blockchains"));
+const TrendingContracts = lazy(() => import("@pages/trending-contracts"));
+const ContractTemplates = lazy(() => import("@pages/contract-templates"));
+const Contracts = lazy(() => import("@pages/contracts"));
 
 const App: React.FC = () => {
   return (
@@ -30,13 +31,15 @@ const App: React.FC = () => {
     >
       <Provider store={store}>
         <HashRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to={BLOCKCHAINS} replace />} />
-            <Route path={BLOCKCHAINS} element={<Blockchains />} />
-            <Route path={POPULAR_CONTRACTS} element={<TrendingContracts />} />
-            <Route path={MY_TEMPLATES} element={<ContractTemplates />} />
-            <Route path={MY_CONTRACTS} element={<Contracts />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Navigate to={BLOCKCHAINS} replace />} />
+              <Route path={BLOCKCHAINS} element={<Blockchains />} />
+              <Route path={POPULAR_CONTRACTS} element={<TrendingContracts />} />
+              <Route path={MY_TEMPLATES} element={<ContractTemplates />} />
+              <Route path={MY_CONTRACTS} element={<Contracts />} />
+            </Routes>
+          </Suspense>
         </HashRouter>
       </Provider>
     </ConfigProvider>
