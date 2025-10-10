44cad9115e65534f373bb155410048e1f17843a5
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-10-10 16:48:19 +0700

    feat: Update collapse icon

--- src/components/abi-form/collapse-form.tsx
+++ src/components/abi-form/collapse-form.tsx
@@ -1,10 +1,19 @@
+import { CaretRightOutlined } from "@ant-design/icons";
 import { Collapse, CollapseProps, Empty } from "antd";
 
 const CollapseForm: React.FC<{ items: CollapseProps["items"] }> = ({
   items,
 }) => {
   if (!items || items.length === 0) return <Empty />;
-  return <Collapse accordion items={items} />;
+  return (
+    <Collapse
+      accordion
+      items={items}
+      expandIcon={({ isActive }) => (
+        <CaretRightOutlined rotate={isActive ? 90 : 0} />
+      )}
+    />
+  );
 };
 
 export default CollapseForm;
